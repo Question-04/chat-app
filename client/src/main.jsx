9eaed3995ce75d42dd16ff476507fb9254b7b5ce
Question-04 <122510061+Question-04@users.noreply.github.com>
2024-11-10 01:42:51 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Toaster } from './components/ui/sonner.jsx';
 import ReactDOM from "react-dom/client"
-import { SocketProvider } from './context/socketContext.jsx';
+import { SocketProvider } from './context/SocketContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
